eacfec2c0ac46972ef527621aa92bedb60171f9e
Heloisag <heloisagvicencio@gmail.com>
2023-12-04 14:01:51 -0300

    feature/hgv/04.12.23/CreatesTheAPIRoute

--- index.js
+++ index.js
@@ -2,11 +2,18 @@ const express = require('express');
 const req = require('express/lib/request');
 const res = require('express/lib/response');
 const server = express();
+const filmes = require('./src/data/filmes.json')
 
-server.get('/', (req, res) => {
-    return res.json({ mensagem: 'API online' })
+server.get('/cartaz', (req, res) => {
+    return res.json({ filmes })
 });
 
-server.listen(3000), () => {
-    console.log('SERVIDOR ESTÁ ONLINE');
-}
+// server.get('/usuarios', (req, res) => {
+//     return res.json({
+//         usuario: 'Heloisa Goulart'
+//     })
+// });
+
+server.listen(3000, () => {
+    console.log('SERVIDOR ESTÁ ONLINE...');
+});
